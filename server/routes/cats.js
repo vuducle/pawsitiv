13f7ff7d00f790568ef61686927a765faed73153
Vu Duc Le <97.vuducle@gmail.com>
2025-04-26 15:07:21 +0200

    Moved route to cats.js

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -1,1 +1,17 @@
-console.log("Home page")
+const express = require("express");
+const router = express.Router();
+
+router.get("/catProfile", (req, res) => {
+  // Render the 'catProfile.hbs' view
+  // Pass the data object (context) to the template
+  res.render("catProfile", {
+    // 'catProfile' refers to views/catProfile.hbs
+    name: "Dave the magical cheese wizard",
+    age: 3,
+    mood: "magical",
+    hungry: "yes",
+    description: "Dave is the most truhest of cats yer eye shall ever see",
+  });
+});
+
+module.exports = router;
