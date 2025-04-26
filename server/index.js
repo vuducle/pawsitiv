83adfc564db9ab86443a483e0718d5ddb3e5fa49
MSSuprise <153920507+MSSurprise@users.noreply.github.com>
2025-04-26 13:51:54 +0200

    created handlebar for cat profile
    I have hopefully created a route and a new hbs. file for the cat.

--- server/index.js
+++ server/index.js
@@ -35,6 +35,19 @@ app.get('/', (req, res) => {
         title: 'Pawsitiv',
     });
 });
+
+// WE WILL ADD MORE ROUTES LATER!!!
+app.get('/catProfile', (req, res) => {
+    // Render the 'catProfile.hbs' view
+    // Pass the data object (context) to the template
+    res.render('catProfile', { // 'catProfile' refers to views/catProfile.hbs
+        name: 'Dave the magical cheese wizard',
+        age: 3,
+        mood: 'magical',
+        hungry: 'yes',
+        description: 'Dave is the most truhest of cats yer eye shall ever see'
+    });
+});
 app.use('/', routes);
 
 app.listen(port, () => {
