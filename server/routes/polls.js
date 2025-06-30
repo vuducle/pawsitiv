5e45e5aa1d00d727f2ae2b61201e39d23c5a99dc
leticia <64483479+leticiahalm@users.noreply.github.com>
2025-06-30 11:37:22 +0200

    added answer poll

--- server/routes/polls.js
+++ server/routes/polls.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const Poll = require('../models/poll');
+const answerRoutes = require('./answers');
 
-//alle Umfragen anzeigen (READ)
+// Alle Umfragen anzeigen (READ)
 router.get('/', async (req, res) => {
     try {
         const polls = await Poll.find();
         res.json(polls);
     } catch (err) {
-        res.status(500).json({error: 'Fehler beim Laden der Umfragen'});
+        res.status(500).json({ error: 'Fehler beim Laden der Umfragen' });
     }
 });
 
-//Neue Umfrage erstellen
+// Neue Umfrage erstellen
 router.post('/', async (req, res) => { 
     const { question } = req.body;
 
-    try{
+    try {
         const newPoll = new Poll({ question });
         const savedPoll = await newPoll.save();
         res.status(201).json(savedPoll);
     } catch (err) {
-        res.status(400).json({ error: 'Fehler beim Erstellen der Umfrage'});
+        res.status(400).json({ error: 'Fehler beim Erstellen der Umfrage' });
     }
- });
+});
 
- module.exports = router;
+// Antworten-Routen als Subrouter mounten
+router.use('/:pollId/answers', answerRoutes);
+
+module.exports = router;
